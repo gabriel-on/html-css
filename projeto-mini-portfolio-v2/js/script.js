5e3f2135fc04c22bcf255a7e4fbb4d232a0eda63
Gabriel ON <gabrielblackbr1@gmail.com>
2023-07-05 17:45:51 -0300

    up

--- projeto-mini-portfolio-v2/js/script.js
+++ projeto-mini-portfolio-v2/js/script.js
@@ -2,3 +2,15 @@ function toggleMode(){
     const html = document.documentElement
     html.classList.toggle('light')
 }
+
+// let toggleMode = document.querySelector('#switch')
+// let ball = document.querySelector(".ball")
+// let dark = document.querySelector('.bi-toggle-off')
+
+// toggleMode.onclick = () => {
+//     ball.classList.toggle('active')
+// }
+
+// localStorage.setItem('pagTema', JSON.stringify(tema))
+
+// let Get <-------- https://youtu.be/-Jt_h91uXkQ
